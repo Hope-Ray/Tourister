44f1b6ba17135f052c9137564bb816e864e3e055
Hope-Ray <anuragshetty007@gmail.com>
2023-02-03 20:56:31 +0530

    commit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -31,7 +31,7 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params;   //ID coming from URL eg: /posts/123  mentioned in routes
     const post = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('NO post available with that ID');
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('NO posts available with that ID');
     
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
     res.json(updatedPost);
