e9f2d11a13362d39f1a20ccea429d25a0396a09e
Hope-Ray <anuragshetty007@gmail.com>
2023-02-07 22:14:38 +0530

    Messed up but seeing if works

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -31,18 +31,32 @@ export const updatePost = async (req, res) => {
     const { id: _id } = req.params;   //ID coming from URL eg: /posts/123  mentioned in routes
     const post = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('NO posts available with that ID');
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('NO post available with that ID');
     
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
     res.json(updatedPost);
 }
 
 export const deletePost = async (req, res) => {
-    const { _id } = req.params;
+    const { id } = req.params;
     
-    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post available with that id');
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post available with that id');
     
-    await PostMessage.findByIdAndRemove(_id);
+    await PostMessage.findByIdAndRemove(id);
+
+    console.log('DELETE');
 
     res.json({message: "Post is deleted Successfully"});
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post available with that id');
+
+    const post = await PostMessage.findById(id);
+    const updatedPost = await  PostMessage.findByIdAndUpdate(id,{ likeCount: post.likeCount + 1 }, {new: true});
+
+    res.json(updatedPost);
+
 }
